d7d56a578f037c4322720c34b49a7662d91e41cf
AdityaTheJaat <programmingID1306@gamil.com>
2023-10-16 08:29:00 +0530

    validations

--- controller/appController.js
+++ controller/appController.js
@@ -3,22 +3,31 @@ require("dotenv").config();
 
 const signup = async (req, res) => {
     const {email, name, number, imgUrl} = req.body;
-    console.log(req)
-	let transporter = nodemailer.createTransport({
-		host: "smtp.gmail.com",
-		port: 465,
-		secure: true, 
-		auth: {
-			user: process.env.MAIL,
-			pass: process.env.PASS, 
-		},
-	});
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true, 
+        auth: {
+            user: process.env.MAIL,
+            pass: process.env.PASS, 
+        },
+    });
     const mailData = {
         from: `${"TEDxLNMIIT 2k23"} <example.com>`,
         to: "theaditya1985@gmail.com",
         subject: `Payment Confirmation email from ${email}`,
         html: `<p>Payment has done by: ${name} <br/> email: ${email}<br/> phoneNumber: ${number}<br/> file link ->${imgUrl}</p>`
     };
+    const mailDataForUser = {
+        from: `${"TEDxLNMIIT 2k23"} <example.com>`,
+        to: `${email}`,
+        subject: `Payment Confirmation email from ${email}`,
+        html: `Payment details are not shared properly please try again`
+    };
+    if(!email || !name || !number || !imgUrl){
+        transporter.sendMail(mailDataForUser);
+        return res.status(400).json({msg: "Please fill all fields"})
+    }
 	transporter
 		.sendMail(mailData)
 		.then((info) => {
