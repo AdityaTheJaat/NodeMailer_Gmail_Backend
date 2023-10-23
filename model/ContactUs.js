6b51c99c680bdb548e7e3c525da29e787e8f0b96
AdityaTheJaat <programmingID1306@gamil.com>
2023-10-23 09:27:31 +0530

    contact us mail sender

---
+++ model/ContactUs.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const { sendMail } = require('../controller/appController');
+const otpTemplate = require('../utils/sendVerificationMail');
+
+const contactSchema = new mongoose.Schema({
+  name:{
+    type:String,
+    required:true
+  },
+  emailBy:{
+    type:String,
+    required:true
+  },
+  message:{
+    type:String,
+  }
+})
+
+contactSchema.pre("save", async function(next){
+  if (this.isNew) {
+    await sendMail(this.email, "Your submitted details", otpTemplate(this.name, this.emailBy, this.message))
+  }  
+  next();
+})
+
+module.exports = mongoose.model("Contact", contactSchema)
